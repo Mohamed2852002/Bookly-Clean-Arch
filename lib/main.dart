c19deeae3f195a0065dc85489039574847f85db8
Mohamed <mohamed2852002@gmail.com>
2024-12-08 15:40:07 +0200

    create simple bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:bookly_clean_arch/core/themes/app_theme.dart';
 import 'package:bookly_clean_arch/core/utils/app_router.dart';
 import 'package:bookly_clean_arch/core/utils/constants.dart';
+import 'package:bookly_clean_arch/core/utils/functions/setup_service_locator.dart';
+import 'package:bookly_clean_arch/core/utils/simple_bloc_observer.dart';
 import 'package:bookly_clean_arch/features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  setupServiceLocator();
   await Hive.initFlutter();
   Hive.registerAdapter(BookEntityAdapter());
   await Hive.openBox<BookEntity>(kFeaturedBooks);
   await Hive.openBox<BookEntity>(kNewestBooks);
-
+  Bloc.observer = SimpleBlocObserver();
   runApp(const Bookly());
 }
 
